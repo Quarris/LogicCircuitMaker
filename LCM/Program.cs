d2c6d6e7f6d201ca3a2ea82f98c4e1be57c326a0
Quarris <tomasz.winnicki@live.co.uk>
2019-09-30 07:36:19 +0100

    Initial Code

---
+++ LCM/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LCM {
+    public static class Program {
+        [STAThread]
+        static void Main(){
+            using (var game = new LCMGame())
+                game.Run();
+        }
+    }
+}
