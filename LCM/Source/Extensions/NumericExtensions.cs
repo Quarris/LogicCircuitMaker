1d27b994878248dbd98ff8d0ba0a9b28b6eda029
Quarris <tomasz.winnicki@live.co.uk>
2020-02-18 18:48:22 +0000

    Added helper method to get direction from vectors

--- LCM/Source/Extensions/NumericExtensions.cs
+++ LCM/Source/Extensions/NumericExtensions.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
 using MLEM.Extensions;
+using MLEM.Misc;
 using MonoGame.Extended;
 
 namespace LCM.Extensions {
     public static class TransformExtensions {
-        public static Size2 Add(this Size2 size, int amount){
+        public static double PiOver8 = Math.PI / 8;
+
+        public static Size2 Add(this Size2 size, int amount) {
             return new Size2(size.Width + amount, size.Height + amount);
         }
 
@@ -26,6 +30,43 @@ namespace LCM.Extensions {
 
         public static Size2 ToSize2(this Size size) {
             return new Size2(size.Width, size.Height);
+        }
+
+        public static Direction2 DirectionTo(this Vector2 origin, Vector2 position) {
+            float angle = position.Translate(-origin.X, -origin.Y).ToAngle();
+            if (angle >= -PiOver8 && angle < PiOver8) {
+                return Direction2.Up;
+            }
+
+            if (angle >= PiOver8 && angle < MathHelper.PiOver2 - PiOver8) {
+                return Direction2.UpRight;
+            }
+
+            if (angle >= MathHelper.PiOver2 - PiOver8 && angle < MathHelper.PiOver2 + PiOver8) {
+                return Direction2.Right;
+            }
+
+            if (angle >= MathHelper.PiOver2 + PiOver8 && angle < MathHelper.Pi - PiOver8) {
+                return Direction2.DownRight;
+            }
+
+            if (angle >= MathHelper.Pi - PiOver8 || angle < -MathHelper.Pi + PiOver8) {
+                return Direction2.Down;
+            }
+
+            if (angle >= -MathHelper.Pi + PiOver8 && angle < -MathHelper.PiOver2 - PiOver8) {
+                return Direction2.DownLeft;
+            }
+
+            if (angle >= -MathHelper.PiOver2 - PiOver8 && angle < -MathHelper.PiOver2 + PiOver8) {
+                return Direction2.Left;
+            }
+
+            if (angle >= -MathHelper.PiOver2 + PiOver8 && angle < -PiOver8) {
+                return Direction2.UpLeft;
+            }
+
+            throw new Exception($"I don't know how you broke this honestly...\n { origin } DirectionTo { position } with angle { angle }");
         }
     }
 }
